e4370c084b83ba23039a1efd69ca7d56cbc0a6b2
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-03-25 12:38:01 +0700

    menggunakan lib tanggal

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,8 @@
+import 'package:dart_lang/dart_lang.dart';
+
 main(List<String> arguments) {
   print("halo ini dari bahasa pemograman dart");
+
+  String tanggalSekarang = formatIndonesianDate(new DateTime.now());
+  print('sekarang tanggal : $tanggalSekarang');
 }
--- lib/dart_lang.dart
+++ lib/dart_lang.dart
@@ -1,3 +1,9 @@
+import 'package:date_format/date_format.dart';
+
 int calculate() {
   return 6 * 7;
+}
+
+String formatIndonesianDate(DateTime date) {
+  return formatDate(date, [dd, '/', MM, '/', yyyy]);
 }
