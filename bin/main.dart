0ba8cc190c4749f6a0596e251132fcb1c0c02a4a
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-03-25 13:46:50 +0700

    beberapa fitur dalam string

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,6 @@
 import 'package:dart_lang/dart_lang.dart';
 import 'variables/number_data_type.dart';
+import 'variables/character_data_type.dart';
 
 main(List<String> arguments) {
   print("halo ini dari bahasa pemograman dart");
@@ -7,5 +8,10 @@ main(List<String> arguments) {
   String tanggalSekarang = formatIndonesianDate(new DateTime.now());
   print('sekarang tanggal : $tanggalSekarang');
 
+  print("--- data type number ---");
   print('nilai dari pi: $phi');
+
+  print("--- data type string ---");
+  print('nilai dari concatnation : $concatnation');
+  print('nilai dari interpolation: $interpolation');
 }
---
+++ bin/variables/character_data_type.dart
@@ -0,0 +1,6 @@
+String _firstName = "Dimas";
+String _lastName = "Maryanto";
+// fitur dalam character atau string
+String concatnation = _firstName + " " + _lastName;
+String interpolation = "halo perkenalkan nama saya $_firstName $_lastName";
+
