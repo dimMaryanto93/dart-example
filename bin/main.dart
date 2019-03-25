8dcef6f2724cd9ceece6099c97cdc9b8581b7d9f
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-03-25 14:15:41 +0700

    membuat fungsi tanggal

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,11 @@
 import 'package:dart_lang/dart_lang.dart';
-import 'variables/number_data_type.dart';
+
 import 'variables/character_data_type.dart';
+import 'variables/date_data_type.dart';
+import 'variables/number_data_type.dart';
 
 main(List<String> arguments) {
   print("halo ini dari bahasa pemograman dart");
-
-  String tanggalSekarang = formatIndonesianDate(new DateTime.now());
-  print('sekarang tanggal : $tanggalSekarang');
 
   print("--- data type number ---");
   print('nilai dari pi: $phi');
@@ -15,4 +14,8 @@ main(List<String> arguments) {
   print('nilai dari concatnation : $concatnation');
   print('nilai dari interpolation: $interpolation');
   print("nilai dari escapeCharacter: $escapeCharacters");
+
+  print("--- data type date ---");
+  var tanggal = formatIndonesianDate(tanggalSekarang);
+  print("nilai dari date : $tanggal");
 }
---
+++ bin/variables/date_data_type.dart
@@ -0,0 +1,2 @@
+DateTime tanggalSekarang = DateTime.now();
+
